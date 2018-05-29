6e879e76d814bdd415b9cba0fe761439eb3c2d2d
deam <deamkh@gmail.com>
2018-05-29 13:01:31 +0200

    Fix minor typo

--- constants.js
+++ constants.js
@@ -20,5 +20,5 @@ module.exports.domains = [
 
 module.exports.max_points = 10000;
 
-module.exports.sheet_id = process.env.NODE_ENV == "production" ? "12cblUYuYq4NwZX7JdRo0-NWnrOxlDy-XCbvF3ugzb2c" : "10sU4UNlCq8fZ3f4zouoq945zTScw27uUV1LU0siA1YA/edit#gid=0";
+module.exports.sheet_id = process.env.NODE_ENV == "production" ? "12cblUYuYq4NwZX7JdRo0-NWnrOxlDy-XCbvF3ugzb2c" : "10sU4UNlCq8fZ3f4zouoq945zTScw27uUV1LU0siA1YA";
 module.exports.sheet_tab_name = "PointsBot (DONT RENAME!)!A1:F1";
