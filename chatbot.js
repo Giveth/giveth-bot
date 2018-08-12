ba67a0080e497cec53a71ef5960e42449922f5a0
Frederik Bolding <frederik.bolding@gmail.com>
2018-08-12 12:21:25 +0200

    Changed all vars to let

--- chatbot.js
+++ chatbot.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const markdown = require("markdown").markdown;
-var privateRooms = {};
+let privateRooms = {};
 
 const { positiveResponses, negativeResponses, messages, questions } = require("./constants");
 
@@ -13,7 +13,7 @@ fs.readFile("./privateRooms.json", "utf8", function (err, data) {
 exports.handleNewMember = function (event, state, member, client) {
   const user = member.userId;
 
-  var roomMessages = messages[state.roomId];
+  let roomMessages = messages[state.roomId];
 
   if (roomMessages && checkUser(user)) {
     handleWelcome(state, user, client, roomMessages.externalMsg, roomMessages.internalMsg);
@@ -22,26 +22,26 @@ exports.handleNewMember = function (event, state, member, client) {
 
 exports.handleResponse = function (event, room, toStartOfTimeline, client) {
   if (event.getType() === "m.room.message" && toStartOfTimeline === false) {
-    var msg = event.getContent().body;
+    let msg = event.getContent().body;
     const user = event.getSender();
 
     if (checkUser(user)) {
 
       if (privateRooms[user] && privateRooms[user].welcoming && user != client.credentials.userId && room.roomId == privateRooms[user].room) {
 
-        var greetingQuestions = messages[privateRooms[user].welcoming.room].internalMsg;
-        var curQuestion = privateRooms[user].welcoming.curQuestion;
+        let greetingQuestions = messages[privateRooms[user].welcoming.room].internalMsg;
+        let curQuestion = privateRooms[user].welcoming.curQuestion;
 
-        var positive = false;
-        var negative = false;
-        for (var p = 0; p < positiveResponses.length; p++) {
+        let positive = false;
+        let negative = false;
+        for (let p = 0; p < positiveResponses.length; p++) {
           if (msg.includes(positiveResponses[p].toLowerCase())) {
             positive = true;
             break;
           }
         }
 
-        for (var n = 0; n < negativeResponses.length; n++) {
+        for (let n = 0; n < negativeResponses.length; n++) {
           if (msg.includes(negativeResponses[n].toLowerCase())) {
             negative = true;
             break;
@@ -65,7 +65,7 @@ exports.handleResponse = function (event, room, toStartOfTimeline, client) {
         }
       } else if ((!privateRooms[user] || !privateRooms[user].welcoming) && user != client.credentials.userId) {
         if (privateRooms[user] && privateRooms[user].room == room.roomId) {
-          for (var key in questions) {
+          for (let key in questions) {
             if (questions.hasOwnProperty(key) && checkForRoomQuestions(msg, key, room.roomId, user, client)) {
               break;
             }
@@ -75,7 +75,7 @@ exports.handleResponse = function (event, room, toStartOfTimeline, client) {
         }
       }
     } else if (event.getType() === "m.room.member" && event.event.membership === "leave") {
-      var privateRoom = privateRooms[event.getSender()];
+      let privateRoom = privateRooms[event.getSender()];
       if (privateRoom && privateRoom.room == event.event.room_id) {
         privateRoom.room = undefined;
         privateRoom.welcoming = undefined;
@@ -91,15 +91,15 @@ function checkUser(user) {
 }
 
 function checkForRoomQuestions(msg, roomForQuestions, roomToSendIn, user, client) {
-  var questionsForRoom = questions[roomForQuestions];
+  let questionsForRoom = questions[roomForQuestions];
   if (questionsForRoom) {
-    for (var i = 0; i < questionsForRoom.length; i++) {
-      var question = questionsForRoom[i];
-      var shouldAnswerQuestion = false;
+    for (let i = 0; i < questionsForRoom.length; i++) {
+      let question = questionsForRoom[i];
+      let shouldAnswerQuestion = false;
       if (typeof question.trigger === "string") {
         shouldAnswerQuestion = msg.toLowerCase().includes(question.trigger.toLowerCase());
       } else {
-        for (var t = 0; t < question.trigger.length; t++) {
+        for (let t = 0; t < question.trigger.length; t++) {
           if (msg.toLowerCase().includes(question.trigger[t].toLowerCase())) {
             shouldAnswerQuestion = true;
             break;
@@ -133,7 +133,7 @@ function sendNextQuestion(curQuestion, questions, user, client, room) {
   if (privateRooms[user]) {
     privateRooms[user].welcoming = { "room": room, "curQuestion": curQuestion };
   }
-  var question = questions[curQuestion];
+  let question = questions[curQuestion];
   sendInternalMessage(question.msg, user, client, () => {
     if (!question.positive) {
       sendNextQuestion(curQuestion, questions, user, client, room);
@@ -163,7 +163,7 @@ function sendInternalMessage(msg, user, client, callback) {
 function sendMessage(msg, user, client, room) {
   if (msg.length > 0) {
     msg = msg.replace(/^ +| +$/gm, "");
-    var html = markdown.toHTML(msg);
+    let html = markdown.toHTML(msg);
     msg = msg.replace("%USER%", user);
     html = html.replace("%USER%", user);
     client.sendHtmlMessage(room, msg, html);
