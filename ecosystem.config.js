a6c462db20feb2a9d3d245c8711583ed96202d3b
Frederik Bolding <frederik.bolding@gmail.com>
2018-10-06 15:10:53 +0200

    Tweaked deployment procedure to allow for bot credential enviroment variables

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,13 +11,9 @@ module.exports = {
         log_date_format: 'YYYY-MM-DD HH:mm',
         env: {
           NODE_ENV: 'debug',
-          BOT_USER: "%USER%",
-          BOT_PASSWORD: "%PASSWORD%"
         },
         env_production: {
           NODE_ENV: 'production',
-          BOT_USER: "%USER%",
-          BOT_PASSWORD: "%PASSWORD%"
         },
       },
     ],
